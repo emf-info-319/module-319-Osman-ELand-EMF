8f88dea537f148767e70a5e0dee043db231401ea
Eland <eland.osman@studentfr.ch>
2024-08-29 11:45:28 +0200

    exercice

---
+++ exercices/exercice03/Exercice3.java
@@ -0,0 +1,11 @@
+package exercices.exercice03;
+
+public class Exercice3.5 {
+    
+    int ageDeEland = 16;
+    int ageDeLucas = 17;
+    String nom = "mon prénom";
+
+    System.out.println("Je m'appelle"+nom+" et j'ai"+ageDeEland);
+    
+}

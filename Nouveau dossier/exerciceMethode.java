3248596464cf2f78027081cb07ca6742bda2657a
Eland <eland.osman@studentfr.ch>
2024-11-21 11:13:16 +0100

    we

--- Nouveau dossier/exerciceMethode.java
+++ Nouveau dossier/exerciceMethode.java
@@ -1,30 +1,25 @@
+import java.util.Random;
+
 public class exerciceMethode {
-
     public static void main(String[] args) {
-
-        int nbreAleatoire();
-
-        String choisirMot();
 
         String[] sujets ={"paule","simon","julie"};
         String[] verbes ={"mange","boit","regarde"};
-        String[] choses ={" une banane "," une maison "," un jus "};
+        String[] choses ={" une banane "," une maison "," un jus " + " une pomme "};
 
-        for (int i = 0; i < choses.length; i++) {
-            System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(choisirMot(sujets)+" "+choisirMot(verbes)+" "+choisirMot(choses));
         }
 
 
 
-    }public static int  nbreAleatoire();{
-        int nbre = ( int ) ( Math.random() * ( 3 - 0 + 1 ) ) + 0;
-        
-    }public static String choisirMot();{
+    }
     
-
-
-
-        
+    public static String choisirMot(String[] mots) {
+        return mots [nbreAleatoire (0, mots.length - 1)];
         
     }
+    public static int  nbreAleatoire(int min, int max) {
+        return ( int ) ( Math.random() * ( max - min + 1 ) ) + min;
+               }
 }

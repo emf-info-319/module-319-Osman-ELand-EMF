1019ca6f3bffa836a7856945a2ea56bb18fb2473
Eland <eland.osman@studentfr.ch>
2024-11-21 09:23:59 +0100

    w

---
+++ E1-pratique/tournoi.java
@@ -0,0 +1,9 @@
+public class tournoi {
+    public final static String EQUIPE = "FC Richemont", "FC Central", "FC Schoenberg", 
+    "FC Beauregard", "Team AFF", "Etoile Sport" ;
+    
+
+    public static void main(String[] args) {
+        
+    }
+}

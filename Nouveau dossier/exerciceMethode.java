33584d675d0144b3274edbc3125c1add8890523d
Eland <eland.osman@studentfr.ch>
2024-11-21 09:55:44 +0100

    we

---
+++ Nouveau dossier/exerciceMethode.java
@@ -0,0 +1,30 @@
+public class exerciceMethode {
+
+    public static void main(String[] args) {
+
+        int nbreAleatoire();
+
+        String choisirMot();
+
+        String[] sujets ={"paule","simon","julie"};
+        String[] verbes ={"mange","boit","regarde"};
+        String[] choses ={" une banane "," une maison "," un jus "};
+
+        for (int i = 0; i < choses.length; i++) {
+            System.out.println();
+        }
+
+
+
+    }public static int  nbreAleatoire();{
+        int nbre = ( int ) ( Math.random() * ( 3 - 0 + 1 ) ) + 0;
+        
+    }public static String choisirMot();{
+    
+
+
+
+        
+        
+    }
+}
